659d491304c9eab937ee6731217ce01c1f55bf4b
tanmt179 <106754526+tanmt179@users.noreply.github.com>
2022-06-08 21:07:48 +0700

    Add files via upload

--- tienjs.js
+++ tienjs.js
@@ -1,21 +1,12 @@
-function changemoney() {
-    let money = document.getElementById("money").value;
-    let from = document.getElementById("from").value;
-    let to = document.getElementById("to").value;
-    let result;
-    if (from == to) {
-        result = "result: " + money;
-    }
-    ;
-    if (from == "VND" && to == "USD") {
-        result = money / 20000;
-    }
-    ;
-    if (from == "USD" && to == "VND") {
-        result = money * 20000;
-    }
-    ;
-    document.getElementById('a').innerHTML = result;
+function change() {
+    let a = +document.getElementById("money").value;
+    let b = +document.getElementById("from").value;
+    let c = +document.getElementById("to").value;
+    d=a*b/c
+
+
+
+    document.getElementById('a').innerHTML = "Result: " + d;
 }
 
 
